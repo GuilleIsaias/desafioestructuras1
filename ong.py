43e8d4403345dbf69f37d29b37d7efd4ffbcfade
Guillermo Conoman <gille.91@hotmail.com>
2024-04-16 18:54:13 -0400

    second commit

---
+++ ong.py
@@ -0,0 +1,10 @@
+import sys
+import math
+
+def facto (**fact_):
+    factorial = math.factorial(fact_)
+    return factorial
+
+factori = facto(sys.argv.append([]))
+
+print(f'El faltorial de numero es {facto()}')
