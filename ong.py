56058d7a57b35b7ab3a89cb6803b587f89ba5649
Guillermo Conoman <gille.91@hotmail.com>
2024-04-16 19:13:34 -0400

    4

--- ong.py
+++ ong.py
@@ -4,7 +4,8 @@ import math
 def facto (**fact_):
     return math.factorial(fact_)
 
-factori = facto(sys.argv[1])
+factori = int(sys.argv[1])
+facto(factori)
 
 
 print(f'El faltorial de numero es {factori}')
